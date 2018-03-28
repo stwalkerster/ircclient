9176036c3413305163bc86c3d26bf3608f34e725
Simon Walker <github@stwalkerster.co.uk>
2018-03-28 22:59:58 +0100

    version numbers

--- Stwalkerster.IrcClient/Properties/AssemblyInfo.cs
+++ Stwalkerster.IrcClient/Properties/AssemblyInfo.cs
@@ -10,4 +10,5 @@
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-dev")]
