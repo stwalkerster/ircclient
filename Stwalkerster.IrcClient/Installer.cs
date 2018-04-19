9acf725be54006f6c123234c88c3207dfa6cdee4
Simon Walker <github@stwalkerster.co.uk>
2018-04-19 22:49:27 +0100

    Add Installer for others to consume services.
    Notably, this does NOT include the main client class, which reusers
    probably want to define themselves with custom configuration.

---
+++ Stwalkerster.IrcClient/Installer.cs
@@ -0,0 +1,17 @@
+﻿namespace Stwalkerster.IrcClient
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+    using Stwalkerster.IrcClient.Interfaces;
+
+    public class Installer : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<ISupportHelper>().ImplementedBy<SupportHelper>()
+            );
+        }
+    }
+}
