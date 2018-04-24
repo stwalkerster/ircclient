1f2e6cc14f69902306754102d2bbf85e93a3da5b
Simon Walker <github@stwalkerster.co.uk>
2018-04-24 20:28:29 +0100

    Add testbot

---
+++ Stwalkerster.IrcClient.Testbot/Startup/Installer.cs
@@ -0,0 +1,25 @@
+﻿namespace Stwalkerster.IrcClient.Testbot.Startup
+{
+    using Castle.Facilities.EventWiring;
+    using Castle.Facilities.Logging;
+    using Castle.Facilities.Startable;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Services.Logging.Log4netIntegration;
+    using Castle.Windsor;
+    using Stwalkerster.IrcClient.Interfaces;
+
+    public class Installer : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<LoggingFacility>(f => f.LogUsing<Log4netFactory>().WithConfig("log4net.xml"));
+            container.AddFacility<EventWiringFacility>();
+            container.AddFacility<StartableFacility>(f => f.DeferredStart());
+
+            container.Install(new Stwalkerster.IrcClient.Installer());
+            container.Register(Component.For<IIrcClient>().ImplementedBy<IrcClient>());
+            container.Register(Component.For<Launch>().ImplementedBy<Launch>());
+        }
+    }
+}
---
+++ Stwalkerster.IrcClient.Testbot/Startup/Launch.cs
@@ -0,0 +1,56 @@
+﻿namespace Stwalkerster.IrcClient.Testbot.Startup
+{
+    using Castle.Core;
+    using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+    using Stwalkerster.IrcClient.Interfaces;
+    using Stwalkerster.IrcClient.Messages;
+
+    public class Launch : IStartable
+    {
+        public static void Main()
+        {
+            var container = new WindsorContainer();
+
+            container.Register(
+                Component.For<IIrcConfiguration>()
+                    .Instance(
+                        new IrcConfiguration(
+                            hostname: "irc.freenode.net",
+                            port: 6667,
+                            authToServices: false,
+                            nickname: "stwtestbot",
+                            username: "stwtestbot",
+                            realName: "stwtestbot",
+                            ssl: false,
+                            clientName: "TestClient"
+                        )));
+            
+            container.Install(FromAssembly.This());
+            
+            var app = container.Resolve<Launch>();
+        }
+
+        public Launch(IIrcClient client)
+        {
+            client.JoinChannel("##stwalkerster-development");
+            client.ReceivedMessage += (sender, args) =>
+            {
+                if (args.Message.Command == "PRIVMSG")
+                {
+                    var message = new Message(args.Message.Command, args.Message.Parameters);
+                    args.Client.Send(message);
+                }
+            };
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
